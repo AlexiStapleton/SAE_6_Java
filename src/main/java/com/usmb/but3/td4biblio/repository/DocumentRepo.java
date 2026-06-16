1f71ca3e1c74467e8e62ddd4462744430ed9982d
TorduGenial <faher.mohamed.pro@gmail.com>
2026-06-16 08:45:06 +0200

    Ajout des méthodes de recherche dans le service et dans la view

--- src/main/java/com/usmb/but3/td4biblio/repository/DocumentRepo.java
+++ src/main/java/com/usmb/but3/td4biblio/repository/DocumentRepo.java
@@ -18,5 +18,28 @@ public interface DocumentRepo extends JpaRepository<Document, Integer> {
 
     List<Document> findByAuteurId(Integer auteurId, Sort sort);
 
+    // Auteur
+    List<Document> findByAuteurNomContainingIgnoreCase(String nom);
+    List<Document> findByAuteurNomStartsWithIgnoreCase(String nom);
+    List<Document> findByAuteurNomIgnoreCase(String nom);
+
+
+    // Bibliothèque
+    List<Document> findByBibliothequeNomContainingIgnoreCase(String nom);
+    List<Document> findByBibliothequeNomStartsWithIgnoreCase(String nom);
+    List<Document> findByBibliothequeNomIgnoreCase(String nom);
+
+
+    // Editeur
+    List<Document> findByEditeurNomContainingIgnoreCase(String nom);
+    List<Document> findByEditeurNomStartsWithIgnoreCase(String nom);
+    List<Document> findByEditeurNomIgnoreCase(String nom);
+
+
+    // Genre
+    List<Document> findByGenreNomContainingIgnoreCase(String nom);
+    List<Document> findByGenreNomStartsWithIgnoreCase(String nom);
+    List<Document> findByGenreNomIgnoreCase(String nom);
+
     List<Document> findAllByOrderByTitreAsc();
 }
