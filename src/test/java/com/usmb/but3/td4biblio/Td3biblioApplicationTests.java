ba807da1a0b60e840b167df9b46248bc4fc3ad85
Stapleton Alexis <155957705+AlexiStapleton@users.noreply.github.com>
2026-06-17 15:54:42 +0200

    Grpschangement pour les test unitaires

--- src/test/java/com/usmb/but3/td4biblio/Td3biblioApplicationTests.java
+++ src/test/java/com/usmb/but3/td4biblio/Td3biblioApplicationTests.java
@@ -1,6 +1,9 @@
 package com.usmb.but3.td4biblio;
 
+import org.flywaydb.core.Flyway;
+import org.hibernate.cfg.Environment;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -11,5 +14,6 @@ class Td3biblioApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
 
 }
