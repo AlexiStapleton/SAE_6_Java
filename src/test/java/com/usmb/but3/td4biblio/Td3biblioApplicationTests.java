16d5bbfd5e3af7b02601aed82a0c72a229526b73
Stapleton Alexis <155957705+AlexiStapleton@users.noreply.github.com>
2026-06-17 12:56:49 +0200

    Bibliotheque crud et fix tests unitaires

--- src/test/java/com/usmb/but3/td4biblio/Td3biblioApplicationTests.java
+++ src/test/java/com/usmb/but3/td4biblio/Td3biblioApplicationTests.java
@@ -2,7 +2,9 @@ package com.usmb.but3.td4biblio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class Td3biblioApplicationTests {
 
